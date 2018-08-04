516c1d5f20e167e4309578578f26a77531e1995a
LoveEmperor <1747098604@qq.com>
2018-08-04 14:27:27 +0800

    init project

---
+++ src/main/java/com/xwz/eurekas/EurekasApplication.java
@@ -0,0 +1,21 @@
+package com.xwz.eurekas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekasApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekasApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekasApplication.class, args);
+	}
+}
